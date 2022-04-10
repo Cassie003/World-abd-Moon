b62730f5f7058d34d4145d9a11b380b9052787fa
Cassie003 <caseyminu@gmail.com>
2022-04-10 09:32:20 +0300

    Addts for quote details

---
+++ src/app/quote/quotes/quotes.component.ts
@@ -0,0 +1,35 @@
+import { Quote } from './../../models/quote';
+import { Component, EventEmitter, Input, OnInit, Output } from '@angular/core';
+
+@Component({
+  selector: 'app-quotes',
+  templateUrl: './quotes.component.html',
+  styleUrls: ['./quotes.component.css']
+})
+export class QuotesComponent implements OnInit {
+
+  @Input() quote: Quote;
+  @Output() deleteQuoteEvent = new EventEmitter<Quote>();
+  @Output() upVoteQuoteEvent = new EventEmitter<Quote>();
+  @Output() downVoteEvent = new EventEmitter<Quote>();
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+  // emit an event to the parent component to delete the quote
+  deleteQuote(quote: Quote) {
+    this.deleteQuoteEvent.emit(quote);
+  }
+
+  // emit an event to the parent component to upvote the quote
+  upVoteQuote(quote: Quote) {
+    this.upVoteQuoteEvent.emit(quote);
+  }
+
+  // emit an event to the parent component to downvote the quote
+  downVoteQuote(quote: Quote) {
+    this.downVoteEvent.emit(quote);
+  }
+
+}
