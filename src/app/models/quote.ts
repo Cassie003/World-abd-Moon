a439a5044492da5f8b22eb943071d3bc64612e55
Cassie003 <caseyminu@gmail.com>
2022-04-10 09:30:17 +0300

    Add  models  for app

---
+++ src/app/models/quote.ts
@@ -0,0 +1,15 @@
+export class Quote {
+  viewQuoteDetails: boolean;
+  randomId = Math.floor(Math.random() * 100);
+  constructor(
+    public id: number,
+    public author: string,
+    public createdBy: string,
+    public quoteDescription: string,
+    public createdAt: Date,
+    public likes: number = 0,
+    public dislikes: number = 0
+  ) {
+    this.viewQuoteDetails = false;
+  }
+  }
